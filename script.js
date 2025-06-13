1cd48211674997ea04e06266e3e117b995748b74
LennartNicklas <lennartnicklas@gmail.com>
2025-06-13 21:37:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js – zukünftige Interaktivität
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Securis Bank Website geladen.");
+    // Hier könnten später Interaktionen folgen
+  });
+  
